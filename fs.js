c40d22876a2e71cb76040fa73689a9bc24e526cd
Prathamesh Naik <cspcce2023@gmail.com>
2022-09-10 14:33:14 +0530

    6th

--- fs.js
+++ fs.js
@@ -1,6 +1,10 @@
 const {readFileSync,writeFileSync}=require('fs')
+console.log('start')
 const first=readFileSync('./content/first.txt','utf-8')
 const second=readFileSync('./content/second.txt','utf-8')
 console.log(first,second)
 
 writeFileSync('./content/result-sysn.txt',`here is the result :${first},${second}`,{flag:'a'})
+console.log('done with task')
+console.log('starting with other task')
+
