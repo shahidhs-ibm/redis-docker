d0dfc164117c69e7c7544b31711faaab6b264df5
Prathamesh Naik <cspcce2023@gmail.com>
2022-09-10 13:22:54 +0530

    2nd commit

--- 02_modules.js
+++ 02_modules.js
@@ -4,3 +4,13 @@ const sayHi = require('./04_utlis')
 
 sayHi(names.john)
 sayHi(names.peter)
+const num1=10
+const num2=20
+
+addvalue=()=>{
+     console.log(num1+num2)
+}
+
+addvalue()
+
+module.exports=addvalue
--- 04_Utlis.js
+++ 04_Utlis.js
@@ -1,5 +1,7 @@
 const sayHi = (name) => {
     console.log(`Hello there ${name}`)
   }
+
+  require('./02_modules')
   // export default
   module.exports = sayHi
