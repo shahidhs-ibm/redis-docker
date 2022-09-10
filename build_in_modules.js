8f043d5ea3c13035096b5bb95e2f6558669c1054
Prathamesh Naik <cspcce2023@gmail.com>
2022-09-10 13:39:01 +0530

    4th

--- build_in_modules.js
+++ build_in_modules.js
@@ -3,3 +3,8 @@ console.log(os.hostname())
 console.log(os.userInfo())
 console.log(os.uptime())
 console.log(os.release())
+
+const path=require('path')
+console.log(path.sep)
+const absolute = path.resolve(__dirname,'node&express','03_names.js')
+console.log(absolute)
